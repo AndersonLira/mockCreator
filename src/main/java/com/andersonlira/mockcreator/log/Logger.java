001eb9342202a1e7af2edf3fab91ec485ddd4795
Anderson Lira <anderson.lira@celfocus.com>
2019-06-04 15:20:06 +0100

    Logger date format altered

--- src/main/java/com/andersonlira/mockcreator/log/Logger.java
+++ src/main/java/com/andersonlira/mockcreator/log/Logger.java
@@ -1,11 +1,12 @@
 package com.andersonlira.mockcreator.log;
 
-
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Logger {
+        private static SimpleDateFormat format = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
         public static void info(Object message){
-                System.out.println(new Date() + " - " + message);
+                System.out.println(format.format(new Date()) + " - " + message);
         }
         public static void info(Object message,Color color){
                 System.out.print(color.toString());
